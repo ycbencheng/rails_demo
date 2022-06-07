ee8ca804329091c14bdaa657ab47faae92a8153a
ycbencheng <ycbencheng@gmail.com>
2022-06-07 16:03:25 -0600

    Update widget validations

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,5 +1,6 @@
 class Widget < ApplicationRecord
   belongs_to :user, foreign_key: :seller_id
 
-  validates :title, :description, :price, presence: true
+  validates :description, :price, presence: true
+  validates :title, presence: true, uniqueness: true
 end
