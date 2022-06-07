2cdd6df29db7ac8e196cc638c8b1adbcff62076f
ycbencheng <ycbencheng@gmail.com>
2022-06-07 16:04:08 -0600

    Create authenticate user method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,8 @@
 class UserController < ApplicationController
   before_action :set_user, only: %i[edit update destroy]
 
+  skip_before_action :authenticate_user, only: [:new, :create]
+
   def new; end
 
   def create
