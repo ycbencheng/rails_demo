4c662fd4174c9da719d376d16f14dec46e0e2b79
ycbencheng <ycbencheng@gmail.com>
2022-06-07 16:28:26 -0600

    Associate widget with transactions

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,5 +1,6 @@
 class Widget < ApplicationRecord
   belongs_to :user, foreign_key: :seller_id
+  has_many :transcations
 
   validates :description, :price, presence: true
   validates :title, presence: true, uniqueness: true
