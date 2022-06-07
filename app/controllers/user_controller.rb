8672b016f2ae06b78e5e98917f5db3a45ec8482c
ycbencheng <ycbencheng@gmail.com>
2022-06-06 19:51:42 -0600

    Update user create method for user sign up

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,18 @@ class UserController < ApplicationController
   def new
   end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      flash[:notice] = "Signup successful!"
+      redirect_to '/signup'
+    else
+      flash[:alert] = "Please try again!"
+      redirect_to '/signup'
+    end
+  end
+
   def edit
 
   end
@@ -13,5 +25,11 @@ class UserController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
