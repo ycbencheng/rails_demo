18ba81a17cbfbe5aaf585af18a250ae5c3c90e1f
ycbencheng <ycbencheng@gmail.com>
2022-06-06 19:11:30 -0600

    Create user model tests

---
+++ test/factories/users.rb
@@ -0,0 +1,10 @@
+require 'ffaker'
+
+FactoryBot.define do
+  factory :user do
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    email { FFaker::Internet.email }
+    password { FFaker::Internet.password }
+  end
+end
