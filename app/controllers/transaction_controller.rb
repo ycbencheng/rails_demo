0826572f2042d31e3ba58979196ce2f458149d87
ycbencheng <ycbencheng@gmail.com>
2022-06-08 10:21:17 -0600

    Create functions to allow user to purchase other widgets

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -3,4 +3,29 @@ class TransactionController < ApplicationController
     @buyer_transactions = current_user.buyer_transactions
     @seller_transactions = current_user.seller_transactions
   end
+
+  def create
+    widget = Widget.find(widget_params[:id])
+    widget_to_transaction = Transaction.widget_to_transaction(widget, current_user)
+    transaction = Transaction.new(widget_to_transaction)
+
+    not_user_widget = widget.seller_id != current_user.id
+
+    if not_user_widget && transaction.save
+      flash[:notice] = "Purchase successful!"
+
+      redirect_to transactions_path
+    else
+      msg = not_user_widget ? "Please try again!" : "Can not purchase your own widget!"
+      flash[:alert] = msg
+
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def widget_params
+    params.require(:widget).permit(:id)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,11 @@ class Transaction < ApplicationRecord
   belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id
   belongs_to :seller, class_name: 'User', foreign_key: :seller_id
   belongs_to :widget
+
+  def self.widget_to_transaction(widget, buyer)
+    { seller_id: widget.seller_id,
+      buyer_id: buyer.id,
+      widget_id: widget.id,
+      amount: widget.price }
+  end
 end
